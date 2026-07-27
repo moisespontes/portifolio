0b830310f0f89b656c9f8e8d89500be0148572b4
moisespontes <moi.pontes@outlook.com>
2026-07-26 22:41:15 -0300

    chore: instala e configura as variáveis de ambiente

--- public_html/index.php
+++ public_html/index.php
@@ -1,7 +1,14 @@
 <?php
 
+use Dotenv\Dotenv;
+
 // Importa o autoload do composer
 require __DIR__ . '/../vendor/autoload.php';
+
+// .env init
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
 // Importa configurações inicias do app
 require __DIR__ . '/../config/bootstrap.php';
 
